d0bc84401f5c3dae5b0505b65903ddcaede0aead
John Njuki <njukijohn2000@gmail.com>
2023-04-12 03:10:28 +0300

    update tests

--- app/src/androidTest/java/com/readingradar/android/ui/screens/home/HomeScreenKtTest.kt
+++ app/src/androidTest/java/com/readingradar/android/ui/screens/home/HomeScreenKtTest.kt
@@ -1,11 +1,8 @@
 package com.readingradar.android.ui.screens.home
 
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.test.assertCountEquals
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
 import com.readingradar.android.R
 import com.readingradar.android.ui.DummyContent
 import org.junit.Rule
@@ -19,7 +16,7 @@ class HomeScreenKtTest {
     @Test
     fun initialState_isRendered() {
         composeTestRule.setContent {
-            HomeScreen(homeUiState = HomeUiState.Loading)
+            HomeScreen(homeUiState = HomeUiState.Loading, onBookCoverClick = {})
         }
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.loading))
             .assertIsDisplayed()
@@ -29,10 +26,25 @@ class HomeScreenKtTest {
     fun stateWithContent_isRendered() {
         val bestSellersList = DummyContent.getBestSellersList()
         composeTestRule.setContent {
-            HomeScreen(homeUiState = HomeUiState.Success(bestSellersList))
+            HomeScreen(homeUiState = HomeUiState.Success(bestSellersList), onBookCoverClick = {})
         }
         composeTestRule.onNodeWithText(bestSellersList[0].displayName).assertIsDisplayed()
         composeTestRule.onAllNodesWithContentDescription("Cover Image")
             .assertCountEquals(bestSellersList.size)
+    }
+
+    @Test
+    fun stateWithContent_ClickOnBookCover_isRegistered() {
+        val bestSellersList = DummyContent.getBestSellersList()
+        val targetBook = bestSellersList[0]
+        composeTestRule.setContent {
+            HomeScreen(
+                homeUiState = HomeUiState.Success(bestSellersList),
+                onBookCoverClick = { isbn ->
+                    assert(isbn == targetBook.books[0].isbn)
+                },
+            )
+        }
+        composeTestRule.onAllNodesWithContentDescription("Cover Image").onFirst().performClick()
     }
 }
